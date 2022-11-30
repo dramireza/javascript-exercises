64816b9059135fce868411e08713e615df4b0da9
David <>
2022-11-30 16:26:44 -0400

    double done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    let reg = pal.replace(/[^\w]/gi,'')
+    let rev = reg.split("").reverse().join("")
+    if ((reg.localeCompare(rev,undefined, {sensitivity:'accent'}))==0)return true;
+    else return false;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a<0) return "OOPS";
+    let previous = 0;
+    let result = 1;
+    for (let index = 1; index < a; index++) {
+        let temp = previous;
+        previous = result;
+        result += temp;
+    }
+    return result;
 };
 
 // Do not edit below this line
