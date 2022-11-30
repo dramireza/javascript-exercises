4fcad299b5fa2bd7210f92552081dc387bac22d3
David <>
2022-11-30 15:26:29 -0400

    Pretty simple math, just had a hitch to remember how to do the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let result = 0;
+  a.forEach(number => {
+    result += number;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1;
+  a.forEach(number => {
+    result *= number;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1;
+  for (let index = 0; index < b; index++) {
+    result *= a;
+  }
+	return result;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let result = 1;
+  for (let index = 1; index <= a; index++) {
+    result *= index;
+  }
+	return result;
 	
 };
 
